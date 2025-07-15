c5a5d6e0a7fc8c50f02153ae53655a6cfa7bfc97
Matus <matus1142@gmail.com>
2025-07-15 18:59:44 +0200

    Add service layer for project

--- src/main/java/com/portfolio/mytaskmanager/service/ProjectService.java
+++ src/main/java/com/portfolio/mytaskmanager/service/ProjectService.java
@@ -1,4 +1,45 @@
 package com.portfolio.mytaskmanager.service;
 
+
+import com.portfolio.mytaskmanager.entity.Project;
+import com.portfolio.mytaskmanager.repository.ProjectRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ProjectService {
+
+    @Autowired
+    private ProjectRepo projectRepo;
+
+    public List<Project> getAllProjects() {
+        return projectRepo.findAll();
+    }
+
+    public Optional<Project> getProjectById(Long id) {
+        return projectRepo.findById(id);
+    }
+
+    public Project createProject(Project project) {
+        return projectRepo.save(project);
+    }
+
+    public Project updateProject(Long id, Project updateProject) {
+        return projectRepo.findById(id)
+                .map(p -> {
+                    p.setName(updateProject.getName());
+                    p.setDescription(updateProject.getDescription());
+                    p.setStartDate(updateProject.getStartDate());
+                    p.setEndDate(updateProject.getEndDate());
+                    return projectRepo.save(p);
+                })
+                .orElseThrow(() -> new RuntimeException("Project not found with id: " + id));
+    }
+
+    public void deleteProject(Long id) {
+        projectRepo.deleteById(id);
+    }
 }
--- src/main/java/com/portfolio/mytaskmanager/service/TaskService.java
+++ src/main/java/com/portfolio/mytaskmanager/service/TaskService.java
@@ -1,4 +1,47 @@
 package com.portfolio.mytaskmanager.service;
 
+
+import com.portfolio.mytaskmanager.entity.Task;
+import com.portfolio.mytaskmanager.repository.TaskRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class TaskService {
+
+    @Autowired
+    private TaskRepo taskRepo;
+
+
+    public List<Task> getAllTasks() {
+        return taskRepo.findAll();
+    }
+
+    public Optional<Task> getTaskById(Long id) {
+        return taskRepo.findById(id);
+    }
+
+    public Task saveTask(Task task) {
+        return taskRepo.save(task);
+    }
+
+    public Task updateTask(Long id, Task updateTAsk) {
+        return taskRepo.findById(id)
+                .map(p -> {
+                    p.setTitle(updateTAsk.getTitle());
+                    p.setDescription(updateTAsk.getDescription());
+                    p.setProject(updateTAsk.getProject());
+                    p.setStatus(updateTAsk.getStatus());
+                    p.setDueDate(updateTAsk.getDueDate());
+                    return taskRepo.save(p);
+                })
+                .orElseThrow(() -> new RuntimeException("Task not found with id: " + id));
+    }
+
+    public void deleteTask(Long id) {
+        taskRepo.deleteById(id);
+    }
 }
